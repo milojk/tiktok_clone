5ae28eb018564a51bedebc09a3b509583f9f0d84
Jessica Jung <nuguyaa@gmail.com>
2024-04-15 12:34:45 +1200

    onboarding feature is added.

---
+++ lib/features/onboarding/tutorial_screen.dart
@@ -0,0 +1,152 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+
+enum Direction { right, left }
+
+enum Page { first, second }
+
+class TutorialScreen extends StatefulWidget {
+  const TutorialScreen({super.key});
+
+  @override
+  State<TutorialScreen> createState() => _TutorialScreenState();
+}
+
+/*
+                      Gaps.v40,
+                      Text(
+                        'Watch cool videos!',
+                        style: TextStyle(
+                          fontSize: Sizes.size32,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Gaps.v20,
+                      Text(
+                        'Videos are personalized for you based on what you watch, like, and share.',
+                        style: TextStyle(
+                          fontSize: Sizes.size16,
+                        ),
+                      ),
+                      Gaps.v24,
+                      */
+
+class _TutorialScreenState extends State<TutorialScreen> {
+  Direction _direction = Direction.right;
+  Page _showingPage = Page.first;
+
+  void _onPanUpdate(DragUpdateDetails details) {
+    if (details.delta.dx > 0) {
+      // to the right
+      if (_direction == Direction.right) return;
+      setState(() {
+        _direction = Direction.right;
+      });
+    } else {
+      if (_direction == Direction.left) return;
+      // to the left
+      setState(() {
+        _direction = Direction.left;
+      });
+    }
+  }
+
+  void _onPanEnd(DragEndDetails details) {
+    if (_direction == Direction.left) {
+      if (_showingPage == Page.second) return;
+      setState(() {
+        _showingPage = Page.second;
+      });
+    } else {
+      if (_showingPage == Page.first) return;
+      setState(() {
+        _showingPage = Page.first;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onPanUpdate: _onPanUpdate,
+      onPanEnd: _onPanEnd,
+      child: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: Sizes.size24,
+          ),
+          child: SafeArea(
+            child: AnimatedCrossFade(
+                firstChild: const Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Gaps.v40,
+                    Text(
+                      'Watch cool videos!',
+                      style: TextStyle(
+                        fontSize: Sizes.size32,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Gaps.v20,
+                    Text(
+                      'Videos are personalized for you based on what you watch, like, and share.',
+                      style: TextStyle(
+                        fontSize: Sizes.size16,
+                      ),
+                    ),
+                    Gaps.v24,
+                  ],
+                ),
+                secondChild: const Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Gaps.v40,
+                    Text(
+                      'Enjoy the ride!',
+                      style: TextStyle(
+                        fontSize: Sizes.size32,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Gaps.v20,
+                    Text(
+                      'Videos will help you be famous!',
+                      style: TextStyle(
+                        fontSize: Sizes.size16,
+                      ),
+                    ),
+                    Gaps.v24,
+                  ],
+                ),
+                crossFadeState: (_showingPage == Page.first)
+                    ? CrossFadeState.showFirst
+                    : CrossFadeState.showSecond,
+                duration: const Duration(
+                  milliseconds: 500,
+                )),
+          ),
+        ),
+        bottomNavigationBar: BottomAppBar(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: Sizes.size24,
+              vertical: Sizes.size1,
+            ),
+            child: AnimatedOpacity(
+              opacity: (_showingPage == Page.first) ? 0 : 1,
+              duration: const Duration(milliseconds: 500),
+              child: CupertinoButton(
+                color: Theme.of(context).primaryColor,
+                child: const Text('Enter the app!'),
+                onPressed: () {},
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
